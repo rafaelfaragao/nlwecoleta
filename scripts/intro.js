e2710a745c68ba9a9fa0e0fe1ff9ecdba5892965
Rafael Aragão <rafaelfaragao@gmail.com>
2020-06-03 20:48:43 -0300

    add scripts js files

---
+++ scripts/intro.js
@@ -0,0 +1,72 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// var ou let e const
+// let myvar = "He" //undefined
+// const myconst = "He" 
+
+// document.write(myvar + myconst)
+
+//string
+// let s1 = "Isso é uma string"
+// let s2 = 'Isso também é uma string'
+// let s3 = `isso é uma string também`
+// document.write(s2)
+
+//number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true or false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+//objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 32,
+//   solteiro: true,
+//   correr(){
+//     //return "run Forest"
+//     document.write("executar uma grande lógica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+
+// const colecaoDeBolinhas = ["blue", "green", 1, {name: "Coiso"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+//Function - funcionalidades - atalhos - reuso de código
+//Função registrada - registrar função
+// function sayMyName(name){
+//   document.write(name + "<br>")
+// }
+// //executar
+// sayMyName("Rafael")
+// sayMyName("Mayk")
+// sayMyName("Thammyres")
+
+//condicionais
+
+// const notaFinal = 4
+
+// if(notaFinal < 5){
+//     //caminho1
+//   document.write("Reprovado")
+// } else {
+//     //caminho2 
+//   document.write("Aprovado")
+// }
+
+//loop - repetições
+
+// for (i=0; i<10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
