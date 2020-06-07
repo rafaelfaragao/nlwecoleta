4c34c2f92250b0f1f54e94a9621e118ab875a3b7
Rafael Aragão <rafaelfaragao@gmail.com>
2020-06-07 11:22:31 -0300

    install nunjucks template engine

--- src/server.js
+++ src/server.js
@@ -2,15 +2,33 @@ const express = require("express")
 
 const server = express()
 
+//configurar pasta public
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjunks = require("nunjucks")
+nunjunks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //configurar caminhos(rotas) da minha aplicação
 //página inicial
 //req : requisição
 //res : resposta
 
 server.get("/", (req,res) =>{
-    res.send("Cheguei aqui")
+    return res.render("index.html")
 })
+
+server.get("/create-point", (req,res) =>{
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req,res) =>{
+    return res.render("search-results.html")
+})
+
 
 //ligar o servidor
 server.listen(3000)
-
