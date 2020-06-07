560d077ac5031aaf636f77bbb005e0ece1099c40
Rafael Aragão <rafaelfaragao@gmail.com>
2020-06-07 09:10:59 -0300

    add package nodemon and root /

--- src/server.js
+++ src/server.js
@@ -2,5 +2,15 @@ const express = require("express")
 
 const server = express()
 
+//configurar caminhos(rotas) da minha aplicação
+//página inicial
+//req : requisição
+//res : resposta
+
+server.get("/", (req,res) =>{
+    res.send("Cheguei aqui")
+})
+
 //ligar o servidor
 server.listen(3000)
+
