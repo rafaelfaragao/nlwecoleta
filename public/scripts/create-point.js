495494ead28df64c1b36ef003f4febee2cb283a1
Rafael Aragão <rafaelfaragao@gmail.com>
2020-06-08 18:44:31 -0300

    add SQLlite file configuration and insert create-point data in database scripts, project NLW Statercomplete

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -67,6 +67,7 @@ function handleSelectItem(event){
 
     const itemId = itemLi.dataset.id
 
+    //console.log('ITEM ID:', itemId)
     //verificar se existem itens selecionados, se sim
     //pegar os itens selecionados
 
@@ -90,6 +91,8 @@ function handleSelectItem(event){
         selectedItems.push(itemId)
     }
 
+    //console.log('selectedItems:',selectedItems)
     //atualizar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems
+
 }
